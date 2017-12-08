c30e61509390ace65d99c0007e2ec533631ab6d2
francescobruschetti <francesco.bruschetti@studenti.unitn.it>
2017-12-08 12:01:32 +0100

    Aggiunta funzionalità push exam

--- index.js
+++ index.js
@@ -10,11 +10,70 @@ var app = express();
  */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+/****************** FINE CONFIGURAZIONE SERVER **********************/
+
+var assignmentIDgenerator = 0;
+var examsDictionary = [ ]; // struttura { studentID:"", assignmentID:"", assignmentContent: obj }];
+
+
+
 
 // disponibilità delle varie aule
-app.all('/aule_libere', function(request, response){
-    response.sendfile('aule_libere.html');
+app.all('/', function(request, response){
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    
+	console.log("Server working properly.");
+	
+    //write response
+    res.write('<h1>IS2 esercizio preparazione esame</h1>');
+    res.write('<p>url: ' + req.url + '</p>');
+    res.write('<p>method: ' + req.method.toLowerCase() + '</p>');
+    res.write('<h3>Operazioni supportate:</h3>');
+	res.write('<p>GET -> studentID=x?assignmentID=y per ottenere il proprio compito \n');
+	res.write('<p>POST -> studentID=x + examContent=object per caricare il proprio compito \n');
+	res.write('<p>PUT -> studentID=x + assignmentID=y per aggiornare il proprio compito \n');
+	res.write('<p>DELETE -> studentID=x + assignmentID=y per eliminare il proprio compito \n');
+
+    //send response
+    res.end();
 });
+
+/** function che permette all'utente di caricare la propria consegna */
+app.post('/upload', function (req, res) {    
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    
+    //process
+    var studentID = (req.query.studentID); // req. --> ottengo il parametro passato dal client
+    var object = parseJson(req.query.object);
+    
+	examsDictionary.push({
+		studentID:  studentID,
+		assignmentID: assignmentIDgenerator,
+		assignmentContent: object
+	});
+	assignmentIDgenerator += 1;
+	
+	
+    //write response
+    res.write('?studentID: ' + examsDictionary + '\n');
+    res.write('?assignmentID: ' + assignmentID + '\n');
+    res.write('obj: ' + obj + '\n');
+	
+    //send response
+    res.end();
+});
+
+
+
+
+
+
+
+
+
+
 
 app.all('/orari', function(req, response, next){ 
 	
